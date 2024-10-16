3d9016cba680ff789c303163f675a3b51a19c3fc
SYM01 <33443792+SYM01@users.noreply.github.com>
2024-10-16 11:06:27 +0000

    replace jest with vitest

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,5 +2,13 @@
 /// <reference types="@types/chrome" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_TITLE: string;
+}
+
+import { RouteLocationNormalizedLoaded, Router } from "vue-router";
+declare module "@vue/runtime-core" {
+  interface ComponentCustomProperties {
+    $router: Router;
+    $route: RouteLocationNormalizedLoaded;
+  }
 }
