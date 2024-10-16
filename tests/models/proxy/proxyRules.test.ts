d415bcc0255731bc935a0b221ac3d2db97d26cd2
SYM01 <33443792+SYM01@users.noreply.github.com>
2024-10-16 19:09:12 +0800

    Fix dependencies issues (#11)
    * modified README.md
    
    * npm audit fix
    
    * remove transifex from Github action. Use Github integration instead
    
    * update deps
    
    * replace jest with vitest

--- tests/models/proxy/proxyRules.test.ts
+++ tests/models/proxy/proxyRules.test.ts
@@ -1,43 +1,47 @@
-import { describe, expect, test } from '@jest/globals';
-import { genSimpleProxyCfg } from "@/models/proxy/proxyRules.ts"
-import { ProfileConfig, SystemProfile } from '@/models/profile';
+import { expect, test, describe } from "vitest";
+import { genSimpleProxyCfg } from "@/models/proxy/proxyRules.ts";
+import { ProfileConfig, SystemProfile } from "@/models/profile";
 
-describe('testing genSimpleProxyCfg for direct and system', () => {
-  test('proxy config mode', () => {
-    const cfg = genSimpleProxyCfg(SystemProfile.DIRECT)
-    expect(cfg.mode).toBe('direct');
+describe("testing genSimpleProxyCfg for direct and system", () => {
+  test("proxy config mode", () => {
+    const cfg = genSimpleProxyCfg(SystemProfile.DIRECT);
+    expect(cfg.mode).toBe("direct");
   });
 });
 
-describe('testing bypass list', () => {
-  test('bypass list with ipv6', () => {
+describe("testing bypass list", () => {
+  test("bypass list with ipv6", () => {
     const profile: ProfileConfig = {
-      profileID: '',
-      color: '',
-      profileName: '',
-      proxyType: 'proxy',
+      profileID: "",
+      color: "",
+      profileName: "",
+      proxyType: "proxy",
       proxyRules: {
         default: {
-          scheme: 'http',
-          host: '127.0.0.1',
+          scheme: "http",
+          host: "127.0.0.1",
           port: 8080,
         },
         https: {
-          scheme: 'direct',
-          host: '',
+          scheme: "direct",
+          host: "",
         },
         bypassList: [
-          '<local>',
-          '127.0.0.1',
-          '192.168.0.1/16',
-          '[::1]',
-          'fefe:13::abc/33',
-        ]
+          "<local>",
+          "127.0.0.1",
+          "192.168.0.1/16",
+          "[::1]",
+          "fefe:13::abc/33",
+        ],
       },
-      pacScript: {}
-    }
-    const cfg = genSimpleProxyCfg(profile)
-    expect(cfg.pacScript?.data).toMatch(/.*?isInNet\(host, '192\.168\.0\.1', '255\.255\.0\.0'\).*?/)
-    expect(cfg.pacScript?.data).toMatch(/.*?isInNet\(host, 'fefe:13::abc', 'ffff:ffff:8000:0:0:0:0:0'\).*?/)
-  })
-})
+      pacScript: {},
+    };
+    const cfg = genSimpleProxyCfg(profile);
+    expect(cfg.pacScript?.data).toMatch(
+      /.*?isInNet\(host, '192\.168\.0\.1', '255\.255\.0\.0'\).*?/
+    );
+    expect(cfg.pacScript?.data).toMatch(
+      /.*?isInNet\(host, 'fefe:13::abc', 'ffff:ffff:8000:0:0:0:0:0'\).*?/
+    );
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,58 +1,74 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { vitePluginForArco } from '@arco-plugins/vite-vue'
-import manifest from './manifest.json'
+/// <reference types="vitest" />
 
-let sourcemap = false
-if (process.env.npm_lifecycle_event?.endsWith(':test')) {
-  sourcemap = true
+import path, { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { vitePluginForArco } from "@arco-plugins/vite-vue";
+import manifest from "./manifest.json";
+
+let sourcemap = false;
+if (process.env.npm_lifecycle_event?.endsWith(":test")) {
+  sourcemap = true;
 }
 
 const getCRXVersion = () => {
   if (process.env.CRX_VER) {
-    let ver = process.env.CRX_VER
-    if (ver.startsWith('v')) {
-      ver = ver.slice(1)
+    let ver = process.env.CRX_VER;
+    if (ver.startsWith("v")) {
+      ver = ver.slice(1);
     }
-    return ver.slice(0, 14)
+    return ver.slice(0, 14);
   }
-  return '0.0.0-dev'
-}
+  return "0.0.0-dev";
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vitePluginForArco({
-      style: 'css',
+      style: "css",
     }),
     {
-      name: 'manifest',
+      name: "manifest",
       generateBundle(outputOption, bundle) {
         const entry = Object.values(bundle).find(
-          (chunk) => chunk.type == 'chunk' && chunk.isEntry && chunk.name == 'background');
-        manifest.version = getCRXVersion().split('-', 1)[0]
-        manifest.version_name = getCRXVersion()
-        manifest.background.service_worker = (entry as any).fileName
+          (chunk) =>
+            chunk.type == "chunk" && chunk.isEntry && chunk.name == "background"
+        );
+        manifest.version = getCRXVersion().split("-", 1)[0];
+        manifest.version_name = getCRXVersion();
+        manifest.background.service_worker = (entry as any).fileName;
 
         this.emitFile({
           type: "asset",
-          fileName: 'manifest.json',
-          source: JSON.stringify(manifest, undefined, 2)
-        })
-      }
-    }
+          fileName: "manifest.json",
+          source: JSON.stringify(manifest, undefined, 2),
+        });
+      },
+    },
   ],
-
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+  test: {},
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'index.html'),
-        popup: resolve(__dirname, 'popup.html'),
-        background: 'src/background.ts',
+        index: resolve(__dirname, "index.html"),
+        popup: resolve(__dirname, "popup.html"),
+        background: "src/background.ts",
       },
     },
-    sourcemap
-  }
-})
+    sourcemap,
+  },
+});
