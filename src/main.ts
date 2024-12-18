3da0afe94ac5f8c21bc54928b6fac16d38141fa2
Michael Huang <michaelhly@gmail.com>
2024-12-18 18:11:39 -0500

    Revert

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,14 @@ import hljs from "highlight.js/lib/core";
 import javascript from "highlight.js/lib/languages/javascript";
 hljs.registerLanguage("javascript", javascript);
 
+const app = createApp(App);
+
 // i18n
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     $t: (key: string, substitutions?: any) => string;
   }
 }
-
-const app = createApp(App);
-
 app.config.globalProperties.$t = Host.getMessage;
 
 app.use(router).mount("#app");
