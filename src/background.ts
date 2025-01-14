1e044bd4b9424440d35fc7efaf6f6d7352f6b38f
SYM01 <33443792+SYM01@users.noreply.github.com>
2025-01-14 11:45:47 +0800

    fix 401 issue (#22)

--- src/background.ts
+++ src/background.ts
@@ -33,7 +33,7 @@ class ProxyAuthProvider {
     callback?: (response: BlockingResponse) => void
   ) {
     if (!details.isProxy) {
-      callback && callback({ cancel: true });
+      callback && callback({});
       return;
     }
 
