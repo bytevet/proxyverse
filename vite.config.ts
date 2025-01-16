a02caba5eda60e59228e0af0a7525ea9ee5f7877
SYM01 <33443792+SYM01@users.noreply.github.com>
2025-01-16 19:24:27 +0800

    optimized firefox build flow

--- vite.config.ts
+++ vite.config.ts
@@ -26,78 +26,105 @@ const getCRXVersion = () => {
 };
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ArcoResolver()],
-    }),
-    Components({
-      resolvers: [
-        ArcoResolver({
-          sideEffect: true,
-        }),
-      ],
-    }),
-    {
-      name: "manifest",
-      generateBundle(outputOption, bundle) {
-        const entry = Object.values(bundle).find(
-          (chunk) =>
-            chunk.type == "chunk" && chunk.isEntry && chunk.name == "background"
-        );
-        manifest.version = getCRXVersion().split("-", 1)[0];
-        manifest.version_name = getCRXVersion();
-        manifest.background.service_worker = (entry as any).fileName;
+export default defineConfig(({ mode }) => {
+  const transformer = TRANSFORMER_CONFIG[mode];
 
-        // To support firefox
-        // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background#browser_support
-        manifest.background.scripts = [(entry as any).fileName];
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ArcoResolver()],
+      }),
+      Components({
+        resolvers: [
+          ArcoResolver({
+            sideEffect: true,
+          }),
+        ],
+      }),
+      {
+        name: "manifest",
+        generateBundle(outputOption, bundle) {
+          const entry = Object.values(bundle).find(
+            (chunk) =>
+              chunk.type == "chunk" &&
+              chunk.isEntry &&
+              chunk.name == "background"
+          );
+          manifest.version = getCRXVersion().split("-", 1)[0];
+          manifest.version_name = getCRXVersion();
+          // avoid cache issues
+          manifest.background.service_worker = (entry as any).fileName;
 
-        this.emitFile({
-          type: "asset",
-          fileName: "manifest.json",
-          source: JSON.stringify(manifest, undefined, 2),
-        });
-      },
-    },
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, "index.html"),
-        popup: resolve(__dirname, "popup.html"),
-        background: "src/background.ts",
-      },
-      output: {
-        manualChunks: {
-          framework: [
-            "vue",
-            "vue-router",
-            "@arco-design/web-vue",
-            "@vueuse/core",
-          ],
+          transformer?.manifest(manifest);
+
+          this.emitFile({
+            type: "asset",
+            fileName: "manifest.json",
+            source: JSON.stringify(manifest, undefined, 2),
+          });
         },
       },
-      plugins: [
-        visualizer({
-          filename: "stats.html",
-          open: true,
-        }),
-      ],
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+        },
+      },
     },
-    sourcemap,
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, "index.html"),
+          popup: resolve(__dirname, "popup.html"),
+          background: "src/background.ts",
+        },
+        output: {
+          manualChunks: {
+            framework: [
+              "vue",
+              "vue-router",
+              "@arco-design/web-vue",
+              "@vueuse/core",
+            ],
+          },
+        },
+        plugins: [
+          visualizer({
+            filename: "stats.html",
+            open: true,
+          }),
+        ],
+      },
+      sourcemap,
+    },
+  };
 });
+
+type Transformer = {
+  manifest(manifest: any): void;
+};
+
+const TRANSFORMER_CONFIG: Record<string, Transformer> = {
+  firefox: {
+    manifest: (manifest) => {
+      // To support firefox
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json/background#browser_support
+      manifest.background.scripts = [manifest.background.service_worker];
+      delete manifest.background.service_worker;
+
+      manifest.browser_specific_settings = {
+        gecko: {
+          id: "proxyverse@byte.vet",
+          strict_min_version: "91.1.0",
+        },
+      };
+    },
+  },
+};
